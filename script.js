bf545a438f9686f5e36874d419dba9d21639968b
CalvinYohanis17 <2272017@maranatha.ac.id>
2026-05-23 18:10:19 +0700

    membuat web dan isi

---
+++ script.js
@@ -0,0 +1,10 @@
+function tampilPesan(era) {
+    
+    if (era === 'aborigin') {
+        alert("Suku Aborigin menggunakan konsep 'Dreamtime' (Waktu Impian) untuk menceritakan bagaimana roh leluhur menciptakan alam semesta, sungai, dan gunung di Australia.");
+    } else if (era === 'modern') {
+        alert("Garis Waktu Singkat:\n1788: Kedatangan narapidana pertama Inggris.\n1851: Demam Emas (Gold Rush) menarik ribuan imigran dunia.\n1901: Australia resmi menjadi satu federasi negara.");
+    } else {
+        alert("Sejarah menarik lainnya akan segera hadir!");
+    }
+}
